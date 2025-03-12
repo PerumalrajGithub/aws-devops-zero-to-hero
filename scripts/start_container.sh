13494c22cdc23a10152e0634e3749247d0c62db3
PerumalrajGithub <163981783+PerumalrajGithub@users.noreply.github.com>
2025-03-12 17:32:15 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Run stop script to clean up previous container
+./stop_container.sh
+
+# Pull the latest Docker image
 docker pull perumalraj2312/simple-python-flask-app:latest
 
-# Run the Docker image as a container
+# Run the container on port 8000
 docker run -d -p 8000:8000 perumalraj2312/simple-python-flask-app:latest
+
+# Show running containers
+docker ps
