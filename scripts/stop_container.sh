a977710e68425d1f9277192ea44ae83769889d69
PerumalrajGithub <163981783+PerumalrajGithub@users.noreply.github.com>
2025-03-12 17:32:34 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# Get the container ID using port 8000
+# Find container using port 8000
 CONTAINER_ID=$(docker ps -q --filter "publish=8000")
 
-# Stop and remove the container if it's running
+# Stop and remove the container if it exists
 if [ ! -z "$CONTAINER_ID" ]; then
-  echo "Stopping and removing container using port 8000: $CONTAINER_ID"
+  echo "Stopping container: $CONTAINER_ID"
   docker stop "$CONTAINER_ID"
   docker rm "$CONTAINER_ID"
 else
